cae2490e460fb6d3defe3119df2bc67e56204e84
Adam <41493452+afl5082@users.noreply.github.com>
2021-08-12 22:06:27 -0400

    Create highest_score_top10.sql

---
+++ highest_score_top10.sql
@@ -0,0 +1,32 @@
+SELECT MAX(score) as max_score,
+        user_id,
+        count(post_id)
+FROM `bigquery-public-data.stackoverflow.comments`
+WHERE extract(year FROM creation_date) = 2020
+GROUP by 2
+ORDER by max_score DESC 
+LIMIT 10;
+
+/*
+SELECT MAX(score) as max_score,
+       a.user_id,
+FROM `bigquery-public-data.stackoverflow.comments` a
+JOIN  
+(
+    SELECT user_id
+    FROM`bigquery-public-data.stackoverflow.comments`
+    GROUP by 1
+    HAVING count(post_id) < 10
+) as b
+ON a.user_id = b.user_id
+WHERE extract(year from creation_date) = 2020
+GROUP BY 2
+ORDER BY 1 DESC 
+LIMIT 10;
+*/
+
+SELECT count(post_id),
+       user_id
+FROM `bigquery-public-data.stackoverflow.comments`
+WHERE user_id IN(4406754,268627,4977243,196678)
+GROUP by 2;
