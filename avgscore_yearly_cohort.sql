c37b2a236c434dc3496d244f05ff1bbcc48afbbd
Adam <41493452+afl5082@users.noreply.github.com>
2021-08-12 21:33:06 -0400

    Update avgscore_yearly_cohort.sql

--- avgscore_yearly_cohort.sql
+++ avgscore_yearly_cohort.sql
@@ -25,3 +25,18 @@ SELECT  signup_year,
                         * 100,2) as pct_change_previous_year
     FROM yearly_cohorts
     ORDER BY 1 ASC; 
+    
+    
+    
+    with q as (
+SELECT  score,
+        NTILE(4) OVER(ORDER BY score) as quartile
+        FROM `bigquery-public-data.stackoverflow.comments` 
+        WHERE score > 0
+        )
+
+        SELECT avg(score) as avg_score,
+                quartile
+        FROM q
+        GROUP BY 2
+       
