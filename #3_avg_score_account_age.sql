fd9ff765066e467df95f124b82c6117faec05da8
Adam <41493452+afl5082@users.noreply.github.com>
2021-08-16 14:55:02 -0400

    Update #3_avg_score_account_age.sql

--- #3_avg_score_account_age.sql
+++ #3_avg_score_account_age.sql
@@ -1,5 +1,5 @@
 SELECT date_diff( current_date(), EXTRACT(DATE FROM u.creation_date), YEAR) as account_age,
-       SUM(c.score) / COUNT(c.post_id) as average_score
+       SUM(c.score) / COUNT(c.id) as average_score
 FROM `bigquery-public-data.stackoverflow.users` u
 JOIN `bigquery-public-data.stackoverflow.comments` c
 ON u.id = c.user_id
