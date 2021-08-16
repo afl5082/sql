31dad91a02d27d348c4b396799e9ea747d702423
Adam <41493452+afl5082@users.noreply.github.com>
2021-08-16 14:05:38 -0400

    Update highest_score_top10.sql

--- highest_score_top10.sql
+++ highest_score_top10.sql
@@ -1,13 +1,4 @@
-SELECT MAX(score) as max_score,
-        user_id,
-        count(post_id)
-FROM `bigquery-public-data.stackoverflow.comments`
-WHERE extract(year FROM creation_date) = 2020
-GROUP by 2
-ORDER by max_score DESC 
-LIMIT 10;
 
-/*
 SELECT MAX(score) as max_score,
        a.user_id,
 FROM `bigquery-public-data.stackoverflow.comments` a
@@ -23,10 +14,24 @@ WHERE extract(year from creation_date) = 2020
 GROUP BY 2
 ORDER BY 1 DESC 
 LIMIT 10;
-*/
+
+
+--quality assurance for above query
+/*
 
 SELECT count(post_id),
        user_id
 FROM `bigquery-public-data.stackoverflow.comments`
 WHERE user_id IN(4406754,268627,4977243,196678)
 GROUP by 2;
+
+
+SELECT MAX(score) as max_score,
+        user_id,
+        count(post_id)
+FROM `bigquery-public-data.stackoverflow.comments`
+WHERE extract(year FROM creation_date) = 2020
+GROUP by 2
+ORDER by max_score DESC 
+LIMIT 10;
+*/
