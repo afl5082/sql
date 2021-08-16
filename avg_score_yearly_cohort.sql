6e1554e0efb28cecc57fe220b7992fd0da9101d7
Adam <41493452+afl5082@users.noreply.github.com>
2021-08-16 11:37:52 -0400

    Update and rename avgscore_yearly_cohort.sql to avg_score_yearly_cohort.sql

--- avg_score_yearly_cohort.sql
+++ avg_score_yearly_cohort.sql
@@ -27,16 +27,4 @@ SELECT  signup_year,
     ORDER BY 1 ASC; 
     
     
-    
-    with q as (
-SELECT  score,
-        NTILE(4) OVER(ORDER BY score) as quartile
-        FROM `bigquery-public-data.stackoverflow.comments` 
-        WHERE score > 0
-        )
-
-        SELECT avg(score) as avg_score,
-                quartile
-        FROM q
-        GROUP BY 2
-       
+   
